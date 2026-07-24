91f8c60017d5a6947e22c20b19f722719f57cca2
Vadaanya Bansal <vadaanyabansal@gmail.com>
2026-07-24 20:19:55 +0530

    done changes

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,1 +1,23 @@
+function Services() {
+  return (
+    <div className="page">
+      <h1>Our Services</h1>
 
+      <p>
+        We provide quality web development solutions using modern technologies.
+      </p>
+
+      <br />
+
+      <ul>
+        <li>🌐 Website Development</li>
+        <li>⚛ React Applications</li>
+        <li>🖥 Frontend Design</li>
+        <li>🛠 Backend Development</li>
+        <li>📱 Responsive Websites</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
