8a3090b71d68c676610ec69144b6eafb0b8a9953
Vadaanya Bansal <vadaanyabansal@gmail.com>
2026-07-24 20:15:20 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import "./App.css";
 
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
