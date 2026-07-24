083924f57fd339ee2a26d9ebd841a3585bf9b96e
Vadaanya Bansal <vadaanyabansal@gmail.com>
2026-07-24 20:19:17 +0530

    done changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,1 +1,24 @@
+function About() {
+  return (
+    <div className="page">
+      <h1>About Us</h1>
 
+      <p>
+        ReactSite is a beginner-friendly project built using React and React
+        Router. It demonstrates how multiple pages can be connected in a Single
+        Page Application.
+      </p>
+
+      <br />
+
+      <h2>Our Mission</h2>
+
+      <p>
+        Our goal is to build simple, modern and user-friendly web applications
+        while learning React concepts step by step.
+      </p>
+    </div>
+  );
+}
+
+export default About;
