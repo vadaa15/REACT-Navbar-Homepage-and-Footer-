41b33b913e821d2cce3921667992656d89d6c833
Vadaanya Bansal <vadaanyabansal@gmail.com>
2026-07-24 20:18:49 +0530

    done changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,25 @@
+function Home() {
+  return (
+    <div className="page">
+      <h1>Welcome to ReactSite</h1>
 
+      <p>
+        This is a simple React Router project where multiple pages are connected
+        using a navigation bar.
+      </p>
+
+      <br />
+
+      <h2>Why Choose Us?</h2>
+
+      <ul>
+        <li>✔ Fast React Application</li>
+        <li>✔ Easy Navigation</li>
+        <li>✔ Beginner Friendly Project</li>
+        <li>✔ Responsive Layout</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
