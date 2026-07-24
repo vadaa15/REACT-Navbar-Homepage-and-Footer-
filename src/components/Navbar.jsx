a9577254d9247481a46e419a6243cf49aa820d86
Vadaanya Bansal <vadaanyabansal@gmail.com>
2026-07-24 20:17:30 +0530

    done changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,34 @@
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
+function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <div className="logo">
+        ReactSite
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+
+    </nav>
+  );
+}
+
+export default Navbar;
