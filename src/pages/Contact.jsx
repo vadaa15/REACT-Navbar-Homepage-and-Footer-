4b3d6a57d287fad89c6dc1229b1285d4c728dab5
Vadaanya Bansal <vadaanyabansal@gmail.com>
2026-07-24 20:20:21 +0530

    done changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,1 +1,21 @@
+function Contact() {
+  return (
+    <div className="page">
+      <h1>Contact Us</h1>
 
+      <p>Email : reactsite@gmail.com</p>
+
+      <p>Phone : +91 9876543210</p>
+
+      <p>Address : Mathura, Uttar Pradesh, India</p>
+
+      <br />
+
+      <h2>Office Hours</h2>
+
+      <p>Monday - Friday : 9:00 AM to 6:00 PM</p>
+    </div>
+  );
+}
+
+export default Contact;
